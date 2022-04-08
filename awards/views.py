49a7cd8cb37095c775c1fb3bb2cac5f1fafb4eff
debbyne <kinyadebbyne@gmail.com>
2022-04-08 22:24:33 +0300

    add signup view functions

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, CommentsForm
+from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth.models import User
+from django.shortcuts import render,redirect, get_object_or_404
+
 
 # Create your views here.
+def SignUp(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'django_registration/registration_form.html', {'form': form})
