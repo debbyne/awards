664a4b045a2973e7d9a13029ae893c39194237e3
debbyne <kinyadebbyne@gmail.com>
2022-04-08 22:09:39 +0300

    add signup form

---
+++ awards/templates/forms.py
@@ -0,0 +1,23 @@
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpForm(forms.ModelForm):
+
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    email = forms.EmailField(max_length=300, help_text='Enter a valid email address')
+    password1 = forms.CharField(max_length=20, widget=forms.PasswordInput,required=True)
+    password2 = forms.CharField(max_length=20, widget=forms.PasswordInput,required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username', 
+            'first_name', 
+            'last_name', 
+            'email', 
+            'password1', 
+            'password2', 
+        ]
