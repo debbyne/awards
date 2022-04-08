dbaa738bf464e7109b51b3829d035817cc0e3fff
debbyne <kinyadebbyne@gmail.com>
2022-04-08 23:55:08 +0300

    add tables to models

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.db import models
 from email.mime import image
 from django.contrib.auth.models import User
@@ -5,6 +6,8 @@ from unicodedata import name
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+import datetime as dt
 # Create your models here.
 
 class Profile(models.Model):
@@ -12,8 +15,6 @@ class Profile(models.Model):
      bio=models.TextField(max_length=400, default="My Bio")
      profile_photo=models.ImageField(upload_to = 'photos/',default="",null=True)
      user=models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-     email = models.EmailField(null=True)
-
      def __str__(self):
         return f'{self.user.username} Profile'
 
@@ -43,3 +44,72 @@ class Profile(models.Model):
 
      def __str__(self):
         return f'{self.user.username} Profile'
+
+class Project(models.Model):
+    image = CloudinaryField('image')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts', null=True)
+    title=models.CharField(max_length=30, blank=True)
+    time_posted=models.DateTimeField(auto_now_add=True, null=True)
+    description = models.TextField(max_length=2000, null=True)
+    url = models.URLField(max_length=80, null=True)
+    class Meta:
+        ordering = ["-pk"]
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
+
+    @classmethod 
+    def get_project_by_user(cls, user):
+        images = cls.objects.filter(user=user)
+        return images
+    def update_project(self, title, description,image):
+        self.title = title
+        self.description = description
+        self.image = image
+        self.save()
+
+    @classmethod
+    def get_all_projects(cls):
+        projects = Project.objects.all()
+        return projects
+
+    @classmethod
+    def search_project_name(cls, search_term):
+        images = cls.objects.filter(
+        title__icontains=search_term)
+        return images
+    @classmethod
+    def get_project_by_user(cls,user_id):
+        projects = Project.objects.filter(user_id=user_id)
+        return projects
+    def _str_(self):
+        return self.user.username
+
+    @classmethod
+    def get_single_project(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
+    def _str_(self):
+        return self.title 
+class Review(models.Model):
+    
+    review = (1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 6),(7, 7),(8, 8),(9, 9),(10, 10)
+    design = models.IntegerField(choices=review,blank=True,default=0)
+    usability = models.IntegerField(choices=review, blank=True,default=0)
+    content = models.IntegerField(choices=review, blank=True,default=0)
+    overall_score = models.IntegerField(blank=True,default=0)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+   
+
+    def __str__(self):
+        return f'{self.project.title} ratings'
+
+    
