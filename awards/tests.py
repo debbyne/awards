c9829c728f6bb1e3b24e3e64120e597ed72faead
debbyne <kinyadebbyne@gmail.com>
2022-04-09 12:51:13 +0300

    add test to project class

--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Project
 
 # Create your tests here.
+class ProjectTestClass(TestCase):
+    def setUp(self):
+       pass
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_image,Project))
+     
+    def test_save_image(self):
+        self.image.save_image()
+        self.assertTrue(len(Project.objects.all()) == 1)
+    def test_delete_image(self):
+        self.test_image.delete_image()
+        image = Project.objects.all()
+        self.assertTrue(len(Project.objects.all()) ==1)
