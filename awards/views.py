cd04a99cebdc05785d231e1c5be6c918c61179ea
debbyne <kinyadebbyne@gmail.com>
2022-04-09 10:20:44 +0300

    add search view function

--- awards/views.py
+++ awards/views.py
@@ -59,3 +59,17 @@ def profile(request):
 def logoutUser(request):
  logout(request)
  return redirect(index)
+
+@login_required(login_url='/accounts/login')
+def search_results(request):
+
+    if 'search_username' in request.GET and request.GET["search_username"]:
+        search_term = request.GET.get("search_username")
+        searched_username = Profile.search_profile(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message, 'profile':searched_username})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
